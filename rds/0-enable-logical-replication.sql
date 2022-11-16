f6bd341888cd6eb106ac5687a07941845edf86ee
Joshua Yoder <me@jpfyoder.com>
2022-11-16 00:25:23 -0500

    feat: better docs, realtime not requirement for script to proceed

--- rds/0-enable-logical-replication.sql
+++ rds/0-enable-logical-replication.sql
@@ -9,7 +9,7 @@ BEGIN
     SELECT setting INTO wal_level FROM pg_settings WHERE name = 'wal_level';
     IF wal_level <> 'logical'
     THEN
-        RAISE EXCEPTION USING MESSAGE = 'RDS logical replication must be enabled to proceed. In other words, the RDS database must be a member of an AWS parameter group with rds.logical_replication = 1. Change the parameter "rds.logical_replication" to "1" in the parameter group, save, and restart the database.';
+        RAISE NOTICE USING MESSAGE = 'RDS logical replication must be enabled to proceed. In other words, the RDS database must be a member of an AWS parameter group with rds.logical_replication = 1. Change the parameter "rds.logical_replication" to "1" in the parameter group, save, and restart the database.';
     END IF;
 END;
 $$;
